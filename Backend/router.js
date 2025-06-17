bf3f35bf5f01e2a508882ef94149a8b7c966d09d
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-17 12:18:17 -0700

    addedCreateCard route and ValidationError custom error

--- Backend/router.js
+++ Backend/router.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const { PrismaClient, Category } = require('./generated/prisma')
+const { ValidationError } = require('./errors')
 const router = express.Router()
 
-router.get('/hello', (req, res) => {
+const prisma = new PrismaClient()
+
+const createCard = async (info) => {
+    const newCard = await prisma.card.create( {data: {
+        ...info,
+        category: Category[info.category.toUpperCase()],
+        cards: []
+    }})
+    return newCard;
+}
+router.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+router.post('/card', (req, res, next) => {
+    const { title, category, cover_img, author } = req.body
+    if( title && Object.values(Category).includes(category) && cover_img && author ) {
+        createCard({ title, category, cover_img, author })
+        return res.status(200).send("Success")
+    }
+    else{
+        const error = new ValidationError("Invalid request")
+        next(error)
+    }
+
 })
 
 module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,18 @@ app.use(express.json())
 
 app.use('/api', router)
 
+app.use((err, req, res, next) => {
+    console.log("in error middle ware")
+    if (err instanceof PrismaClientKnownRequestError){
+        if (err.code === 'P2002'){
+            return res.status(400).json({ error: "A unique constraint violation occurred." })
+        }
+    }
+    else {
+        return res.status(err.statusCode).json({error: err.message}).send()
+    }
+})
+
 app.listen(PORT, () => {
     console.log("app started on http://localhost:" + PORT + " !")
 })
