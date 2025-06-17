162da65e6d3e827474f13d517672a86c3e4bd754
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-17 12:38:54 -0700

    Created HomeBoardsContainer

--- kudosboard/src/App.tsx
+++ kudosboard/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/home/NavBar'
+import HomeBoardsContainer from './HomeBoardsContainer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
   return (
     <div>
         <NavBar />
+        <HomeBoardsContainer />
     </div>
   )
 }
---
+++ kudosboard/src/HomeBoardsContainer.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+const HomeBoardsContainer = () => {
+
+    useEffect(() =>{
+
+    }, [])
+    return(
+        <>  Boards Container </>
+    )
+}
+
+export default HomeBoardsContainer
