0881ec1f47c2ed7ed1dc465ce09cb12bb8fcd262
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-18 12:29:59 -0700

    Added getBoards fetch method to initial useEffect + Wireframed cards to display the content

--- kudosboard/src/HomeBoardsContainer.tsx
+++ kudosboard/src/HomeBoardsContainer.tsx
@@ -1,12 +1,55 @@
-import { useEffect } from "react"
+import { useContext, useEffect } from "react"
+import { AppContext } from "./AppContext"
+import { getBoards } from "./fetchRequests"
 
 const HomeBoardsContainer = () => {
-
+    const { boards, setBoards } = useContext(AppContext)
     useEffect(() =>{
-
+        getBoards().then((boards) => {
+            setBoards(boards)
+        })
     }, [])
+
+    useEffect(() => {
+        console.log(boards)
+    }, [boards])
+
+    if(!boards){
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    if(boards.length === 0){
+        return(
+            <div>
+                No boards found
+            </div>
+        )
+    }
     return(
-        <>  Boards Container </>
+        <div className='flex flex-row flex-wrap justify-around mt-5 gap-5'>
+            {boards.map((board: any) => {
+                return(
+                    <div className=' flex flex-col items-center w-48 p-1 border-black border-1 hover:scale-105 transition-transform' key={board.id}>
+                        <img className='overflow-clip w-44 h-80' src={board.coverImg} />
+                        <p className=' py-1 font-bold'>{board.title}</p>
+                        <p>{board.author}</p>
+                        <div className="flex flex-row">
+                            <button>
+                                View Board
+                            </button>
+                            <button>
+                                Delete Board
+                            </button>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
