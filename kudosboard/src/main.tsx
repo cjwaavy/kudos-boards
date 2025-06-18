3546b95e6e44a4a5612fc0de70d5d351fa56be8e
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-18 11:13:20 -0700

    Added Context Proivder for the app

--- kudosboard/src/main.tsx
+++ kudosboard/src/main.tsx
@@ -3,14 +3,19 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import BoardPage from './BoardPage.tsx'
+import { AppContext } from './AppContext.tsx'
+import { useState } from 'react'
 
+const [boards, setBoards] = useState();
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/boards" element={<BoardPage />} />
-      <Route path="*" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+  <AppContext value={ { boards, setBoards } }>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/boards" element={<BoardPage />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </AppContext>
 )
