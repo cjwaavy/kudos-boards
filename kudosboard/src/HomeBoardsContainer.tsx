5a1b0a9dd53bad7a265b20888796a31692f3c573
Charles Hendrid McClasky <cmcclasky@meta.com>
2025-06-18 12:46:13 -0700

    Added more styling on Boards in HomeBoardsContainter

--- kudosboard/src/HomeBoardsContainer.tsx
+++ kudosboard/src/HomeBoardsContainer.tsx
@@ -32,15 +32,15 @@ const HomeBoardsContainer = () => {
         <div className='flex flex-row flex-wrap justify-around mt-5 gap-5'>
             {boards.map((board: any) => {
                 return(
-                    <div className=' flex flex-col items-center w-48 p-1 border-black border-1 hover:scale-105 transition-transform' key={board.id}>
+                    <div className=' flex flex-col items-center w-68 p-1 hover:scale-105 transition-transform' key={board.id}>
                         <img className='overflow-clip w-44 h-80' src={board.coverImg} />
                         <p className=' py-1 font-bold'>{board.title}</p>
                         <p>{board.author}</p>
-                        <div className="flex flex-row">
-                            <button>
+                        <div className="flex flex-row gap-10 justify-evenly py-1">
+                            <button className='!bg-gray-100 dark:!bg-slate-800 dark:!text-white !text-black underline'>
                                 View Board
                             </button>
-                            <button>
+                            <button className='!bg-teal-800'>
                                 Delete Board
                             </button>
                         </div>
