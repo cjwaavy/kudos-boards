9f6bfb0ccbb3c4766fde6a6cc74aba7314870710
Charles Hendrid McClasky <cjmcclasky@gmail.com>
2025-06-19 14:55:57 -0700

    added Back Button to BoardPage

--- kudosboard/src/BoardPage.tsx
+++ kudosboard/src/BoardPage.tsx
@@ -1,7 +1,28 @@
-const BoardPage = () => {
-    return (
-        <div>Board Page</div>
-    )
-}
+import { useNavigate } from "react-router";
 
-export default BoardPage
+
+const BoardPage = () => {
+
+    let navigate = useNavigate()
+    const handleGoBack = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="mb-6">
+                <button
+                    onClick={handleGoBack}
+                    className="flex items-center px-4 py-2 bg-teal-600 text-white rounded hover:bg-teal-700 transition-colors"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
+                    </svg>
+                    Back to Home
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default BoardPage;
