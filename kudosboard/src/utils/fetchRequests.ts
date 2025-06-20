79161991e3337de58de2d00ccde17cdcf03803d1
Charles Hendrid McClasky <cjmcclasky@gmail.com>
2025-06-20 14:43:20 -0700

    added get Comments fetch request in frontend

--- kudosboard/src/utils/fetchRequests.ts
+++ kudosboard/src/utils/fetchRequests.ts
@@ -1,26 +1,26 @@
-// import process from "process";
+// import process from "process"
 const serverUrl = import.meta.env.VITE_API_URL
 
 const getBoards = async () => {
     try {
-        const response = await fetch(`${serverUrl}/api/boards`);
+        const response = await fetch(`${serverUrl}/api/boards`)
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        return data;
+        const data = await response.json()
+        return data
     }
     catch (err) {
-        console.error("Error fetching boards: ", err);
-        return null;
+        console.error("Error fetching boards: ", err)
+        return null
     }
-};
+}
 
 const createBoard = async (boardData: {
-    title: string;
-    category: string;
-    coverImg: string;
-    author?: string | null;
+    title: string
+    category: string
+    coverImg: string
+    author?: string | null
 }) => {
     try {
         const response = await fetch(`${serverUrl}/api/boards`, {
@@ -29,61 +29,61 @@ const createBoard = async (boardData: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(boardData),
-        });
+        })
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        return data;
+        const data = await response.json()
+        return data
     } catch (err) {
-        console.error("Error creating board: ", err);
-        return null;
+        console.error("Error creating board: ", err)
+        return null
     }
-};
+}
 
 const deleteBoard = async (boardId: number) => {
     try {
         const response = await fetch(`${serverUrl}/api/boards/${boardId}`, {
             method: 'DELETE',
-        });
+        })
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        return true;
+        return true
     } catch (err) {
-        console.error("Error deleting board: ", err);
-        return null;
+        console.error("Error deleting board: ", err)
+        return null
     }
-};
+}
 
 const getCards = async (boardId: number) => {
     try {
-        const response = await fetch(`${serverUrl}/api/boards/${boardId}/cards`);
+        const response = await fetch(`${serverUrl}/api/boards/${boardId}/cards`)
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        return data;
+        const data = await response.json()
+        return data
     } catch (err) {
-        console.error(`Error fetching cards for board ${boardId}: `, err);
-        return null;
+        console.error(`Error fetching cards for board ${boardId}: `, err)
+        return null
     }
-};
+}
 
 const deleteCard = async (boardId:number, cardId: number) => {
     try {
         const response = await fetch(`${serverUrl}/api/boards/${boardId}/cards/${cardId}`, {
             method: 'DELETE',
-        });
+        })
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        return true;
+        return true
     } catch (err) {
-        console.error(`Error deleting card ${cardId}: `, err);
-        return null;
+        console.error(`Error deleting card ${cardId}: `, err)
+        return null
     }
-};
+}
 
 const upvoteCard = async (boardId: number, cardId: number) => {
     try {
@@ -92,24 +92,24 @@ const upvoteCard = async (boardId: number, cardId: number) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-        });
+        })
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        return data;
+        const data = await response.json()
+        return data
     } catch (err) {
-        console.error(`Error upvoting card ${cardId}: `, err);
-        return null;
+        console.error(`Error upvoting card ${cardId}: `, err)
+        return null
     }
-};
+}
 
 
 const createCard = async (boardId: number, cardData: {
-    title: string;
-    description: string;
-    gifUrl: string;
-    owner?: string | null;
+    title: string
+    description: string
+    gifUrl: string
+    owner?: string | null
 }) => {
     try {
         const response = await fetch(`${serverUrl}/api/boards/${boardId}/cards`, {
@@ -118,16 +118,30 @@ const createCard = async (boardId: number, cardData: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(cardData),
-        });
+        })
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        return data
+    } catch (err) {
+        console.error("Error creating card: ", err)
+        return null
+    }
+}
+
+const getComments = async (boardId: number, cardId: number) => {
+    try {
+        const response = await fetch(`${serverUrl}/api/boards/${boardId}/cards/${cardId}/comments`);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
         return data;
     } catch (err) {
-        console.error("Error creating card: ", err);
+        console.error(`Error fetching comments for card ${cardId}: `, err);
         return null;
     }
 };
 
-export { getBoards, createBoard, deleteBoard, getCards, deleteCard, upvoteCard, createCard };
+export { getBoards, createBoard, deleteBoard, getCards, deleteCard, upvoteCard, createCard, getComments };
